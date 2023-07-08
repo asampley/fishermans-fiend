348a045923a0d1788f5be31e5e29babb9764b991
dadames <dadames999@gmail.com>
2023-07-08 12:49:08 -0600

    Obstacles will bounce and Enemies will damage

--- Assets/GMTK2023/Scripts/Units/Enemy/EnemyManager.cs
+++ Assets/GMTK2023/Scripts/Units/Enemy/EnemyManager.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class EnemyManager : UnitManager, ICollidable
 {
+    private Enemy _enemy;
+
+    public void Initialize(Enemy enemy, bool movesFromLeft)
+    {
+        _enemy = enemy;
+
+        base.Initialize(enemy, movesFromLeft);
+    }
+
     public void Collide(Tentacle tentacle)
     {
         tentacle.Blocked();
+        GameManager.Instance.PlayerTakeDamage(_enemy.AttackDamage);
     }
 
     public void Collide(ProjectileManager projectile)
